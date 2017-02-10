145a6a006bd709cde8b52e4f20219d6cd65b40b0
Chris Umbel <chris@chrisumbel.com>
2017-02-10 07:47:25 -0500

    started controllers

---
+++ src/StatlerWaldorfCorp.EcommerceCatalog/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StatlerWaldorfCorp.EcommerceCatalog.Controllers
+{
+    [Route("api/products")]    
+    public class ProductsController : Controller    
+    {
+        public ProductsController()
+        {
+
+        }
+    }
+}
