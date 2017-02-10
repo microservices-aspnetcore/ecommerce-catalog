787ac27f52f7560d291820dc6786dfa17e9aed7c
Chris Umbel <chris@chrisumbel.com>
2017-02-10 08:35:12 -0500

    model

--- src/StatlerWaldorfCorp.EcommerceCatalog/Models/Product.cs
+++ src/StatlerWaldorfCorp.EcommerceCatalog/Models/Product.cs
@@ -2,6 +2,7 @@ namespace StatlerWaldorfCorp.EcommerceCatalog.Models
 {
     public class Product
     {
+        public int SKU { get; set; }        
         public string Name { get; set; }
     }
 }
