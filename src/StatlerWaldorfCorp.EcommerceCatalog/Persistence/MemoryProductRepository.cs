4f30ae6f2c352a9b12e2516a7bf5e322b7457097
autodidaddict <alothien@gmail.com>
2017-03-12 16:37:34 -0400

    Adding fake products

--- src/StatlerWaldorfCorp.EcommerceCatalog/Persistence/MemoryProductRepository.cs
+++ src/StatlerWaldorfCorp.EcommerceCatalog/Persistence/MemoryProductRepository.cs
@@ -5,9 +5,27 @@ namespace StatlerWaldorfCorp.EcommerceCatalog.Persistence
 {
     public class MemoryProductRepository : IProductRepository
     {
+        private List<Product> products;
+
+        public MemoryProductRepository()
+        {
+            products = new List<Product>();
+
+            products.Add(new Product
+            {
+                SKU = 123,
+                Name = "The Magic 123"
+            });
+
+            products.Add(new Product
+            {
+                SKU = 456,
+                Name = "Supervac"
+            });
+        }
         public ICollection<Product> All()
         {
-            return new List<Product>();
+            return products;
         }
     }
 }
